5887db2115a9cb1903d473f982250b2513d13791
Rudy Sicard <rudy.sicard@mlstate.com>
2012-03-06 21:46:32 +0100

    [feature] Initial commit

---
+++ gotoOpaDoc.py
@@ -0,0 +1,17 @@
+import sublime
+import sublime_plugin
+import webbrowser
+
+class gotoOpaDocCommand(sublime_plugin.TextCommand):
+
+    def run(self, edit):
+        word = self.view.substr(self.view.word(self.view.sel()[0].begin()))
+        for region in self.view.sel():
+            docapi = 'http://doc.opalang.org/api'
+            #docapisearch = 'http://doc.opalang.org/search?q='
+            docapisearch = 'http://doc.opalang.org/#!/search/'
+            if not region.empty():
+                phrase = self.view.substr(region)
+                webbrowser.open_new_tab(docapisearch + phrase)
+            else:
+                webbrowser.open_new_tab(docapi)
