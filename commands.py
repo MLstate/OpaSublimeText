0809b69237c55f0098071e8d6492f355f9f9d526
Rudy Sicard <rudy.sicard@mlstate.com>
2012-07-05 18:18:08 +0200

    [fix] kill: better kill, pid based

--- commands.py
+++ commands.py
@@ -1,53 +1,114 @@
 import os
 import sublime, sublime_plugin
 import sys
+import task, subprocess, signal
 
 basename_exe = "opa_build"
-exe = basename_exe+".exe"
+
+if sys.platform.startswith('win'):
+	exe_name = basename_exe+".js"
+else:
+	exe_name = basename_exe+".exe"
+
+launched = {}
+locations = {}
+
+def prog_name(prog):
+	if sys.platform.startswith('win'):
+		return prog+".exe"
+	else:
+		return prog
+
+def location(program):
+	if not (program in locations):
+		for path in os.environ["PATH"].split(os.pathsep):
+			pfile = os.path.join(path, program)
+			if os.path.isfile(pfile):
+				locations[program] = path
+				return path
+		raise Exception(program,'Not found')
+	else:
+		return locations[program]
+
+def taskid(self,dirname,exe):
+	return dirname
+	#return str(self.view.id())
 
 def kill(self,dirname,exe):
-	if sys.platform.startswith('win'):
-		self.view.window().run_command('exec', {'cmd': ["tskill "+os.path.splitext(exe)[0]], 'working_dir':dirname, 'shell':True} )
+	id = taskid(self,dirname,exe)
+	print "Killing task" , id
+	if not(id in launched):
+		print "Nothing to do"
+		return
+	t = launched.pop(id)
+	if hasattr(t,"proc"): 
+		pid = t.proc.proc.pid
 	else:
-		self.view.window().run_command('exec', {'cmd': ["killall "+exe], 'working_dir':dirname, 'shell':True} )
-
+		pid = None
+	if pid==None:
+		print "Nothing to do (No pid)"
+		return
+	if t.proc.proc.returncode!=None:
+		print "Nothing to do (Already returned)"
+		return
+	print "Killing pid" , pid
+	if sys.platform.startswith('win'):
+		si = subprocess.STARTUPINFO()
+		si.dwFlags = subprocess.STARTF_USESHOWWINDOW
+		si.wShowindow = subprocess.SW_HIDE
+		subprocess.call(['taskkill','/F', '/T', '/PID', str(pid)],startupinfo=si)
+	else:
+		os.killpg(pid, signal.SIGKILL)
+	
 def launch(self,dirname,exe):
 	exe = os.path.join(dirname,exe)
+	command = exe+" -p 2000"
+	env = os.environ.copy()
+	working_dir = dirname
 	if sys.platform.startswith('win'):
+		node = prog_name("node")
 		node_path = os.path.join(os.getenv("APPDATA"),"npm","node_modules")
-		os.environ["NODE_PATH"] = node_path
-		self.view.window().run_command('exec', {'cmd': ["node "+exe+" -p 2000"], 'working_dir':dirname, 'shell':True} )
+		env["NODE_PATH"] = node_path
+		command = node+ " " + command
 	else:
-		self.view.window().run_command('exec', {'cmd': [exe+" -p 2000"], 'working_dir':dirname, 'shell':True} )
+		command =  command
+	t = task.Task(self.view.window())
+	id = taskid(self,dirname,exe)
+	launched[id] = t
+	t.run(cmd=[command],working_dir=dirname,env=env)
+	launched[id] = t
+	print "Has launched", t.proc.proc.pid
 
-class RunOpaBuildCommand(sublime_plugin.TextCommand):
+def output(view,str):
+	print str
+	selection_was_at_end = (len(view.sel()) == 1 and view.sel()[0] == sublime.Region(view.size()))
+	view.set_read_only(False)
+	edit = view.begin_edit()
+	view.insert(edit, view.size(), str)
+	if selection_was_at_end:
+		view.show(view.size())
+	view.end_edit(edit)
+	view.set_read_only(True)
+
+class runOpaBuildCommand(sublime_plugin.TextCommand):
 	def run(self, view):
+		panel = self.view.window().get_output_panel("exec")
 		dirname = os.path.dirname(self.view.file_name())
-		print "Will build & run", exe
+		exe = exe_name
+		output(panel, "Will build & run "+exe)
+		output(panel,"\nCompiling ...")
 		self.view.window().run_command('build')
+		output(panel,"done\n")
+		output(panel,"\nStoping previous launch ...")
 		kill(self,dirname,exe)
+		output(panel,"done\n")
+		output(panel,"Launching ...")
 		launch(self,dirname,exe)
+		#output(panel,"The application has started\n")
 
-class StopRunOpaBuildCommand(sublime_plugin.TextCommand):
+
+class stopRunOpaBuildCommand(sublime_plugin.TextCommand):
 	def run(self, view):
 		dirname = os.path.dirname(self.view.file_name())
-		print "Will kill", exe
-		kill(self,dirname,exe)
-
-#class CheckProjectCommand(sublime_plugin.WindowCommand):
-#	def run(self, edit):
-#		#get filename
-		#for root, dirs, files in os.walk("."):
-#			print "ROOT " , root
-#	     	print "dirs " , dirs
-#	     	print "files ", files
-	    # 	[dirs.remove(dir) for dir in dirs if dir in dirs_to_ignore]
-	    # 	paths = dirs
-	    # 	paths.extend(files)
-	    # 	for path in paths:
-	    #     	print "path = $path"
-	    #     	print "fullpath = $full_path"
-    	#     	full_path = os.path.join(root, path)	
-     #    return
-
-
+		print "Will kill", exe_name
+		kill(self,dirname,exe_name)
